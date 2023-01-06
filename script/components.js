188206f2c14e68db0c447a165ebf77f096f7c9e2
himu <mmpliuhuan@outlook.com>
2023-01-06 16:35:44 +0800

    Cookie added. Modal Completed.

--- script/components.js
+++ script/components.js
@@ -1,14 +1,65 @@
 // noinspection JSJQueryEfficiency
 
-/* According media screen to change some style */
-const mQuery = window.matchMedia('(max-width: 768px)');
-/* preload background */
-let img = new Image();
-img.src = "../images/backgrounds/Dark/background1.jpg";
+/**
+ * @author Himu
+ * @file components.js
+ * @brief 本文件描述了所有控件的行为
+ */
 
-/* load theme according to time */
-// 0 -- Light, 1 -- Dark;
-let gWebsiteThemeFlag = 0;
+/* According media screen to change some style */
+const mScreenWidth = window.matchMedia('(max-width: 768px)');
+
+$(document).click(function () {
+    $(".home-searchbar").css("width", "250px");
+    $(".bgbox").css({
+        "transform": "scale(1.0)",
+        "filter": "blur(0px)"
+    });
+    $(".daily-words").css("display", "none");
+    $("#home-timer").css("display", "block");
+    $(".home-sidebar").css("width", "0px");
+})
+
+$("#nav-index").click(function () {
+    $(".dropdown-menu").slideDown(300);
+});
+
+/* mobile nav */
+$("#nav-menu-button").click(function (event) {
+    $("#nav-sidebar").css({
+        "display": "flex",
+        "width": "400px"
+    });
+    event.stopPropagation();
+})
+
+/* sidebar */
+$("#nav-sidebar").mouseover(function () {
+    if (!mScreenWidth.matches) {
+        $("#nav-sidebar").css({
+            "width": "150px",
+            "height": "100vh",
+            "top": "0",
+            "position": "fixed"
+        });
+        $("#sidebar-tip").fadeOut("slow");
+    }
+}).mouseout(function () {
+    if (!mScreenWidth.matches) {
+        $("#nav-sidebar").css({
+            "width": "0px",
+            "height": "100%",
+            "top": "0",
+            "position": "fixed"
+        });
+    }
+});
+
+$(".dropdown-menu").mouseleave(function () {
+    $(".dropdown-menu").slideUp(600);
+}).mousedown(function () {
+    $(".dropdown-menu").slideUp(300);
+});
 
 /**
  * daily words gen.
@@ -28,109 +79,27 @@ let dailyWords = [
     "将爱形容为美丽的，是不了解爱的人；将爱形容为丑陋的，是自以为了解爱的人。"
 ];
 
-function changeTheme() {
-    changeBgBoxImage();
-    if (gWebsiteThemeFlag === 0) {
-        $("#main-nav").addClass("bg-dark").removeClass("bg-white").removeClass("opacity-85")
-            .removeClass("navbar-light").addClass("navbar-dark");
-        $("#dropdown-list").addClass("dropdown-menu-dark");
-        document.documentElement.style.setProperty("--bs-body-color", "#ffffff");
-        document.documentElement.style.setProperty("--bs-body-bg", "#333333");
-        document.documentElement.style.setProperty("--bs-secondary", "#e5e5e5");
-        gWebsiteThemeFlag = 1;
-    } else {
-        $("#main-nav").addClass("bg-white").removeClass("bg-dark").addClass("opacity-85")
-            .removeClass("navbar-dark").addClass("navbar-light");
-        $("#dropdown-list").removeClass("dropdown-menu-dark");
-        document.documentElement.style.setProperty("--bs-body-color", "#000000");
-        document.documentElement.style.setProperty("--bs-body-bg", "#ffffff");
-        document.documentElement.style.setProperty("--bs-secondary", "#9f9f9f");
-        gWebsiteThemeFlag = 0;
+
+$("#search-bar").click(function (event) {
+    if (mScreenWidth.matches) {
+        $("#home-timer").css("display", "none");
     }
-}
-
-$(document).ready(function () {
-    let hour = new Date().getHours();
-    if (hour >= 17 || hour <= 5) {
-        console.log("set to dark");
-        changeTheme();
-        gWebsiteThemeFlag = 1;
+    $(".home-searchbar").css("width", "500px");
+    $(".daily-words").fadeIn(2000)
+        .text(dailyWords[Math.round(Math.random() * dailyWords.length)]);
+    $(".tip-words").fadeOut("slow");
+    $(".bgbox").css({
+        "transform": "scale(1.1)",
+        "filter": "blur(20px)"
+    });
+    event.stopPropagation();
+}).on("keypress", function (event) {
+    if (event.keyCode === 13) {
+        let url = "https://cn.bing.com/search?q=";
+        url += $("#search-bar").val();
+        window.open(url, "_blank");
+        $("#search-bar").val("");
     }
-    /* mobile nav */
-    $("#nav-menu-button").click(function (event) {
-        $("#nav-sidebar").css({
-            "display": "flex",
-            "width": "400px"
-        });
-        event.stopPropagation();
-    })
-    /* timer */
-    updateTimer();
-
-    /* sidebar */
-    $("#nav-sidebar").mouseover(function () {
-        if (!mQuery.matches) {
-            $("#nav-sidebar").css({
-                "width": "150px",
-                "height": "100vh",
-                "top": "0",
-                "position": "fixed"
-            });
-            $("#sidebar-tip").fadeOut("slow");
-        }
-    }).mouseout(function () {
-        if (!mQuery.matches) {
-            $("#nav-sidebar").css({
-                "width": "0px",
-                "height": "100%",
-                "top": "0",
-                "position": "fixed"
-            });
-        }
-    });
-
-    $("#nav-index").click(function () {
-        $(".dropdown-menu").slideDown(300);
-    });
-
-    $(".dropdown-menu").mouseleave(function () {
-        $(".dropdown-menu").slideUp(600);
-    }).mousedown(function () {
-        $(".dropdown-menu").slideUp(300);
-    });
-
-    $("#search-bar").click(function (event) {
-        if (mQuery.matches) {
-            $("#home-timer").css("display", "none");
-        }
-        $(".home-searchbar").css("width", "500px");
-        $(".daily-words").fadeIn(2000)
-            .text(dailyWords[Math.round(Math.random() * dailyWords.length)]);
-        $(".tip-words").fadeOut("slow");
-        $(".bgbox").css({
-            "transform": "scale(1.1)",
-            "filter": "blur(20px)"
-        });
-        event.stopPropagation();
-    }).on("keypress", function (event) {
-        if (event.keyCode === 13) {
-            let url = "https://cn.bing.com/search?q=";
-            url += $("#search-bar").val();
-            window.open(url, "_blank");
-            $("#search-bar").val("");
-        }
-    });
-
-    $(document).click(function () {
-        $(".home-searchbar").css("width", "250px");
-        $(".bgbox").css({
-            "transform": "scale(1.0)",
-            "filter": "blur(0px)"
-        });
-        $(".daily-words").css("display", "none");
-        $("#home-timer").css("display", "block");
-        $(".home-sidebar").css("width", "0px");
-    })
 });
 
 /* Autoplay backgrounds  */
@@ -162,10 +131,62 @@ function updateTimer() {
     $("#home-timer").text(timer());
 }
 
-function changeBgBoxImage() {
+setInterval(updateTimer, 10000);
+
+/* load theme according to time */
+let gWebsiteThemeFlag;
+
+function getThemeFlag() {
+    if ($.cookie('theme') === undefined) {
+        console.log("[User Cookie]: set to system theme.");
+        const themeMedia = window.matchMedia("(prefers-color-scheme: light)");
+        $.cookie('theme', (themeMedia.matches) ? 'light' : 'dark', {expires: 7, path: '/'});
+        return 0;
+    } else
+        return ($.cookie('theme') === 'light') ? 0 : 1;
+}
+
+function setTheme() {
+    gWebsiteThemeFlag = getThemeFlag();
+    console.log(gWebsiteThemeFlag);
+    if (gWebsiteThemeFlag === 1) {
+        console.log("[User Cookie]: set to dark theme");
+        $("#main-nav").addClass("bg-dark").removeClass("bg-white")
+            .removeClass("navbar-light").addClass("navbar-dark");
+        $("#dropdown-list").addClass("dropdown-menu-dark");
+        $(".himu-card").addClass("himu-card-dark");
+        document.documentElement.style.setProperty("--bs-body-color", "#ffffff");
+        document.documentElement.style.setProperty("--bs-body-bg", "#1f1f1f");
+        document.documentElement.style.setProperty("--bs-secondary", "#e5e5e5");
+    } else {
+        console.log("[User Cookie]: set to light theme");
+
+        $("#main-nav").addClass("bg-white").removeClass("bg-dark").addClass("opacity-85")
+            .removeClass("navbar-dark").addClass("navbar-light");
+        $(".himu-card").removeClass("himu-card-dark");
+        $("#dropdown-list").removeClass("dropdown-menu-dark");
+        document.documentElement.style.setProperty("--bs-body-color", "#000000");
+        document.documentElement.style.setProperty("--bs-body-bg", "#ffffff");
+        document.documentElement.style.setProperty("--bs-secondary", "#9f9f9f");
+    }
+    setBgBoxImage();
+}
+
+function changeTheme() {
+    if (gWebsiteThemeFlag === 0) {
+        $.cookie('theme', 'dark', {path: '/', expires: 7});
+        gWebsiteThemeFlag = 1;
+    } else {
+        $.cookie('theme', 'light', {path: '/', expires: 7});
+        gWebsiteThemeFlag = 0;
+    }
+    setTheme();
+}
+
+function setBgBoxImage() {
     for (let i = 0; i < backgroundBoxes.length; i++) {
         let src = backgroundBoxes[i].getAttribute("src");
-        if (gWebsiteThemeFlag === 0) {
+        if (gWebsiteThemeFlag === 1) {
             backgroundBoxes[i].setAttribute("src",
                 src.replace("Light", "Dark"));
         } else {
@@ -181,10 +202,42 @@ function closeBanner(name) {
 
 function showModal(name) {
     $(name).fadeIn(500);
+    setActiveModalTab("#modal-copyright-tab");
 }
 
 function hideModal(name) {
     $(name).fadeOut(300);
 }
 
-setInterval(updateTimer, 10000);
+function setActiveModalTab(name) {
+    $('.himu-modal-sidebar-link').removeClass("himu-active-link");
+    let linkName = name + "-link";
+    $(linkName).addClass("himu-active-link");
+    $('.himu-modal-tab').hide();
+    $(name).fadeIn(500);
+}
+
+function preloadBackground() {
+    /* preload background */
+    let images = [];
+    function preload() {
+        for (let i = 0; i < preload.arguments.length; i++) {
+            images[i] = new Image();
+            images[i].src = preload.arguments[i];
+            console.log(`image ${preload.arguments[i]} loaded`);
+        }
+    }
+
+    preload(
+        "../images/backgrounds/Light/background1.jpg",
+        "../images/backgrounds/Light/background2.jpg",
+        "../images/backgrounds/Light/background3.jpg",
+        "../images/backgrounds/Light/background4.jpg",
+        "../images/backgrounds/Dark/background1.jpg",
+        "../images/backgrounds/Dark/background2.jpg",
+        "../images/backgrounds/Dark/background3.jpg",
+        "../images/backgrounds/Dark/background4.jpg",
+        "../images/backgrounds/Login/LoginBackground.jpg"
+    );
+}
+
